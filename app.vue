0bf240acae4e67799b7ccc935dd9e12240fffda2
CristopherMartins <cmartins@bonadmin.com>
2023-04-13 18:27:18 -0300

    💫 update and add page/layout transition on app.vue file

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style lang="scss">
+    .page-enter-active,
+    .page-leave-active {
+        transition: $default_transition;
+    }
+    .page-enter-from,
+    .page-leave-to {
+        opacity: 0;
+        // filter: blur(1rem);
+    }
+    .layout-enter-active,
+    .layout-leave-active {
+        transition: $default_transition;
+    }
+    .layout-enter-from,
+    .layout-leave-to {
+        opacity: 0;
+        // filter: blur(1rem);
+    }
+</style>
