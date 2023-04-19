20406f47f844431bd4c5a337b72eca667e764d0f
Cristopher <cristophermartins.77@gmail.com>
2023-04-19 10:28:53 -0300

    🚧 update components

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,16 @@
 <template>
-    <header>
-        <TheLogo />
+    <header class="header">
+        <div class="container__fluid">
+            <TheLogo />
+        </div>
     </header>
 </template>
+
+<style lang="scss" scoped>
+    .header{
+        padding: 20px 0;
+        .container__fluid{
+            display: flex;
+        }
+    }
+</style>
