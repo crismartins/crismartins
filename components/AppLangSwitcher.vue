d7a8e89b63793a662c0e456445b7d6557221dd99
CristopherMartins <cmartins@bonadmin.com>
2023-04-24 18:53:42 -0300

    lang switcher in progress

---
+++ components/AppLangSwitcher.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li>
+            English
+        </li>
+        <li>
+            Português
+        </li>
+    </ul>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
