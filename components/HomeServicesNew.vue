3588204f76779b04f70893a90b8e810550275f3f
CristopherMartins <cmartins@bonadmin.com>
2023-08-16 18:08:16 -0300

    finish services section

--- components/HomeServicesNew.vue
+++ components/HomeServicesNew.vue
@@ -22,20 +22,15 @@
                     >
                         <header class="services__section__container__column__details__item__header">
                             <figure>
-                                <!-- <img 
-                                    :src="service.image"
-                                    width="240"
-                                    height="386"
-                                /> -->
+                                <AppIcon :IconName="service.icon" />
                             </figure>
-                            <h3>
-                                {{ service.title }}
-                            </h3>
                         </header>
                         <div 
-                            :id="'service_'+service.id"
                             class="services__section__container__column__details__item__content"
                         >
+                            <h4>
+                                {{ service.title }}
+                            </h4>
                             <p>
                                 {{ service.description }}
                             </p>
@@ -55,24 +50,18 @@ import { reactive} from '#imports'
 
 let services = reactive([
     {
-        title: 'Branding',
-        image: '/images/crismartins.png',
-        description: 'UX and UI Design for Web Applications, Mobile Applications, Softwares or Systems.'
-    }
-    ,
-    {
-        title: 'UX',
-        image: '/images/crismartins.png',
+        title: 'Graphics',
+        icon: 'lucide:paint-bucket',
         description: 'UX and UI Design for Web Applications, Mobile Applications, Softwares or Systems.'
     },
     {
-        title: 'UI',
-        image: '/images/crismartins.png',
+        title: 'UX & UI',
+        icon: 'lucide:pencil-ruler',
         description: 'UX and UI Design for Web Applications, Mobile Applications, Softwares or Systems.'
     },
     {
         title: 'Development',
-        image: '/images/crismartins.png',
+        icon: 'lucide:code-2',
         description: 'UX and UI Design for Web Applications, Mobile Applications, Softwares or Systems.'
     }
 
@@ -82,7 +71,7 @@ let services = reactive([
 
 <style lang="scss" scoped>
 .services__section{
-    padding-block: 40px;
+    padding-block: 80px;
     &__container{
         display: flex;
         gap: 40px;
@@ -91,7 +80,8 @@ let services = reactive([
             transform: rotate(-180deg);
         }
         h3{
-            margin-bottom: 40px;
+            position:relative; 
+            z-index: 2;
         }
         &__column{
             display: grid;
@@ -102,51 +92,108 @@ let services = reactive([
                 position: relative;
                 &:before{
                     content: '';
-                    background: var(--secondary);
+                    background: var(--tertiary);
                     position: absolute;
-                    opacity: 0.6;
+                    opacity: 0.4;
                     top: 20%;
                     left: 5%;
                     width: 50%;
                     aspect-ratio: 1;
                     border-radius: 100%;
-                    filter: blur(120px);
-                    box-shadow:  240px 20px 0 0 var(--neutral), 80px 240px 0 0 var(--tertiary);
+                    filter: blur(180px);
+                    box-shadow:  240px 20px 0 0 var(--secondary), 80px 240px 0 0 var(--primary);
                 }
             }
 
             &__details{
                 position: relative;
-                display: grid;
-                grid-template-columns:repeat(3, 1fr);
+                display: flex;
+                // display: grid;
+                // grid-template-columns:repeat(3, 1fr);
                 gap: 12px;
                 &__item{
-                    background: var(--secondary);
-                    border-radius: 40px;
-                    padding: 40px;
+                    border-radius: 120px;
+                    padding: 20px;
                     position: relative;
                     display: flex;
                     flex-direction: column;
+                    align-items: center;
                     justify-content: space-between;
-                    box-shadow: 0 12px 20px 0 var(--text_color_transparent);
-                    background-color: var(--pure_white);
+                    border: 1px solid var(--text_color_transparent);
+                    min-width: 200px;
+                    transition: $transition_default;
+                    &__header{
+                        width: 100%;
+                        figure{
+                            background-color: var(--bg_color_transparent);
+                            aspect-ratio: 1;
+                            border-radius: 50%;
+                            display: grid;
+                            place-items: center;
+                            font-size: 48px;
+                            color: var(--text_color);
+                            width: 120px;
+                            margin: 0;
+                            margin: auto;
+                        }
+                    }
+                    &__content{
+                        margin-block: 40px;
+                        h4{
+                            font-size: 24px;
+                            font-weight: 400;
+                        }
+                        p{
+                            color: var(--text_color);
+                            margin-bottom: 40px;
+                        }
+                        button{
+                            margin: auto;
+                        }
+                    }
+                    // box-shadow: 0 12px 20px 0 var(--text_color_transparent);
+                    // background-color: var(--pure_white);
                     &:nth-child(1){
-                        grid-area: 1 / 1 / 3 / 1;
-                        // background: linear-gradient(45deg, #ff5e23 40%, #f4f973 100%);
+                        // grid-area: 1 / 1 / 3 / 1;
+                        figure{
+                            // color: var(--tertiary);
+                            
+                            background: linear-gradient(45deg, #ff4fd0 40%, #f4f973 100%);
+                            box-shadow: 0 20px 0 0 rgba(#ff4fd0, 0.4),
+                            0 40px 0 0 rgba(#ff4fd0, 0.2);
+                            color: rgba(white, 0.8);
+                        }
                     }
                     &:nth-child(2){
-                        // background: linear-gradient(45deg, #b01ad9 40%, #ff34c9 100%);
+                        figure{
+                            // color: var(--secondary);
+                            
+                            background: linear-gradient(45deg, #b01ad9 40%, #ff34c9 100%);
+                            box-shadow: 0 20px 0 0 rgba(#b01ad9, 0.4),
+                            0 40px 0 0 rgba(#b01ad9, 0.2);
+                            color: rgba(white, 0.8);
+                        }
                     }
                     &:nth-child(3){
-                        // background: linear-gradient(45deg, #ff4fd0 40%, #f4f973 100%);
+                        figure{
+                            // color: var(--primary);
+                            background: linear-gradient(45deg, #083dec 40%, #31eaa0 100%);
+                            box-shadow: 0 20px 0 0 rgba(#083dec, 0.4),
+                            0 40px 0 0 rgba(#083dec, 0.2);
+                            color: rgba(white, 0.8);
+                            
+                        }
                     }
                     &:nth-child(4){
-                        grid-area: 2 / 2 / 2 / 4;
-                        // background: linear-gradient(45deg, #083dec 40%, #31eaa0 100%);
+                        // grid-area: 2 / 2 / 2 / 4;
+                        figure{
+                            background: linear-gradient(45deg, #ff5e23 40%, #f4f973 100%);
+                            
+                        }
                     }
-                    // &:last-child{
-                    //     grid-column: 1 / 4;
-                    // }
+                    &:last-child{
+                        // grid-column: 1 / 4;
+                    }
                     // &:before{
                     //     content: '';
                     //     background: var(--primary);
@@ -160,20 +207,7 @@ let services = reactive([
                     //     filter: blur(100px);
                     //     box-shadow:  240px 20px 0 0 var(--secondary), 80px 240px 0 0 var(--tertiary);
                     // }
-                    &__header{
-                        figure{
-
-                        }
-                        h3{
-
-                        }
-                    }
-                    &__content{
-                        position: relative;
-                        z-index: 1;
-                        width: 100%;
-                        color: var(--text_color);
-                    }
+                    
                 }
             }
         }
